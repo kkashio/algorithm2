0a162769ec990149a5a5c0688934c436d198466d
박성민 <seongmin@bagseongmin-ui-MacBook-Pro.local>
2018-01-29 12:58:18 +0900

    first commit

---
+++ src/Q2357.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Q2357 {
+    static int n,m,arr[];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt(); m=sc.nextInt();
+        int i;
+        arr = new int[n];
+        for(i=0; i<n; i++)
+            arr[i] = sc.nextInt();
+
+
+
+    }
+}
